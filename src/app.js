93c9eca4b9b70630a46956f5cad1c85447d57852
Helio Lima <heliolima.work@gmail.com>
2020-04-20 23:31:38 -0300

    Aplicação teste para criar repositórios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,62 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title} = request.query;
+  const results = title ? repositories.filter( repo => repo.title.includes(title)) : repositories;
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const {url, title, techs} = request.body;
+    const repo = { id: uuid(), url, title, techs, likes: 0};
+    repositories.push(repo);
+    return response.json(repo);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const { url, title, techs} = request.body;
+  const repoIndex = repositories.findIndex(repo => repo.id == id);
+  
+  if(repoIndex < 0){
+      return response.status(400).json({error: "Repository not found"});
+  }
+  
+  const likes = repositories[repoIndex].likes;
+  const updatedRepo = {id, url, title, techs, likes};
+  repositories[repoIndex]= updatedRepo;
+
+  return response.json(updatedRepo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id == id);
+  
+  if(repoIndex < 0){
+      return response.status(400).json({error: "Repository not found"});
+  }
+
+  repositories.splice(repoIndex, 1);
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id == id);
+  
+  if(repoIndex < 0){
+    return response.status(400).json({error: "Repository not found"});
+  }
+  
+  const oldRepo = repositories[repoIndex];
+  const {title, url, techs, likes}  = oldRepo;
+  const updatedRepo = {id, url, title, techs, likes: likes+1};
+  repositories[repoIndex]= updatedRepo;
+
+  return response.json(updatedRepo);
 });
 
 module.exports = app;
